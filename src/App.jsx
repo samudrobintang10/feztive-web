5ef23dd4d11d65fdbf2f5ed10ff682c0db13c1d9
Bintang Samudro <samudrobintang10@gmail.com>
2024-05-11 21:07:52 +0700

    change router behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Home from "./views/Home";
+import About from "./views/About";
+import Event from "./views/Event";
+import Merchandise from "./views/Merchandise";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import ResetPassword from "./views/ResetPassword";
+import DashboardCatalog from "./views/DashboardCatalog";
+
 function App() {
-  return <div className="App">Coba testing untuk disini</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Navigate to="/" />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/merchandise" element={<Merchandise />} />
+          <Route path="/dashboard-catalog" element={<DashboardCatalog />} />
+        </Route>
+        <Route path="/" element={<GuestLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/password/reset" element={<ResetPassword />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import "./index.css";
 import router from "./router.jsx";
 import { RouterProvider } from "react-router-dom";
 import { ContextProvider } from "./contexts/ContextProvider.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ContextProvider>
-      <RouterProvider router={router} />
+      <App />
     </ContextProvider>
   </React.StrictMode>
 );
